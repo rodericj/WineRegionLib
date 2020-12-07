b8b9f3da44ddbb1b1e0408fc30377e0415256f51
Roderic Campbell <roderic@gmail.com>
2020-12-06 22:39:42 -0800

    Add all of Italy (large change), update USA and France

--- Sources/WineRegionLib/Regions/France.swift
+++ Sources/WineRegionLib/Regions/France.swift
@@ -61,91 +61,90 @@ public enum BordeauxSubRegion: String, WineRegionDescribable, Hashable {
     }
 }
 
-extension WineCountry {
-    public struct France {
-        public static let title = "France"
-        public struct Burgundy {
-            public static let title = "Burgundy"
+public struct France {
+    public static let title = "France"
+    public struct Burgundy {
+        public static let title = "Burgundy"
+        public enum Appelation: String, AppelationDescribable {
+            public var description: String {
+                switch self {
+                case .coteDeBeaune,
+                     .volay1erCru,
+                     .aloxCorton1erCru: return "Beaune"
+                case .notStJulien: return "Saint Julien"
+                }
+            }
+
+            public var url: URL {
+                switch self {
+                case .notStJulien:
+                    return URL(string: "https://github.com/rodericj/BordeauxWineRegions/raw/master/St-Julien-AOP_Bordeaux_France.geojson")!
+                case .coteDeBeaune, .volay1erCru, .aloxCorton1erCru:
+                    return URL(string: "https://github.com/rodericj/WineRegionMaps/raw/main/France/Burgundy/CoteDeBeaune.geojson")!
+                }
+            }
+            case notStJulien
+            case coteDeBeaune
+            case volay1erCru = "Volnay 1er Cru"
+            case aloxCorton1erCru = "Aloxe-Corton 1er Cru"
+        }
+    }
+    public struct Bordeaux {
+        public static let title = "Bordeaux"
+        public struct Medoc {
+            public static let title = "Medoc"
             public enum Appelation: String, AppelationDescribable {
+                // Note: The is used as geojson's first feature's property's name
                 public var description: String {
                     switch self {
-                    case .coteDeBeaune,
-                         .volay1erCru,
-                         .aloxCorton1erCru: return "Beaune"
-                    case .notStJulien: return "Saint Julien"
+                    case .medoc: return "Medoc"
+                    case .stJulien: return "Saint Julien"
+                    case .hautMedoc: return "Haut-Medoc"
+                    case .stEsteph: return "Saint-Estephe"
+                    case .margaux: return "Margaux"
+                    case .moulisEnMedoc: return "Moulis-en-Médoc"
+                    case .pauillac: return "Pauillac"
+                    case .sauternes: return "Sauternes"
+                    case .graves: return "Graves"
                     }
                 }
 
                 public var url: URL {
+                    let rootURLString = "https://github.com/rodericj/BordeauxWineRegions/raw/master/"
+                    let fileName: String
                     switch self {
-                    case .notStJulien:
-                        return URL(string: "https://github.com/rodericj/BordeauxWineRegions/raw/master/St-Julien-AOP_Bordeaux_France.geojson")!
-                    case .coteDeBeaune, .volay1erCru, .aloxCorton1erCru:
-                        return URL(string: "https://github.com/rodericj/WineRegionMaps/raw/main/France/Burgundy/CoteDeBeaune.geojson")!
+                    case .stJulien:
+                        fileName = "St-Julien-AOP_Bordeaux_France.geojson"
+                    case .medoc:
+                        fileName = "Medoc-AOP_Bordeaux_France.geojson"
+                    case .hautMedoc:
+                        fileName = "Haut-Medoc-AOP_Bordeaux_France.geojson"
+                    case .stEsteph:
+                        fileName = "St-Estephe-AOP_Bordeaux_France.geojson"
+                    case .margaux:
+                        fileName = "Margaux-AOP_Bordeaux_France.geojson"
+                    case .moulisEnMedoc:
+                        fileName = "Moulis-en-Medoc-AOP_Bordeaux_France.geojson"
+                    case .pauillac:
+                        fileName = "Pauillac-AOP_Bordeaux_France.geojson"
+                    case .sauternes:
+                        fileName = "Sauternes-PDO_Bordeaux_France.geojson"
+                    case .graves:
+                        fileName = "Graves-AOP_Bordeaux_France.geojson"
                     }
+                    return URL(string: "\(rootURLString)/\(fileName)")!
                 }
-                case notStJulien
-                case coteDeBeaune
-                case volay1erCru = "Volnay 1er Cru"
-                case aloxCorton1erCru = "Aloxe-Corton 1er Cru"
-            }
-        }
-        public struct Bordeaux {
-            public static let title = "Bordeaux"
-            public struct Medoc {
-                public static let title = "Medoc"
-                public enum Appelation: String, AppelationDescribable {
-                    // Note: The is used as geojson's first feature's property's name
-                    public var description: String {
-                        switch self {
-                        case .medoc: return "Medoc"
-                        case .stJulien: return "Saint Julien"
-                        case .hautMedoc: return "Haut-Medoc"
-                        case .stEsteph: return "Saint-Estephe"
-                        case .margaux: return "Margaux"
-                        case .moulisEnMedoc: return "Moulis-en-Médoc"
-                        case .pauillac: return "Pauillac"
-                        case .sauternes: return "Sauternes"
-                        case .graves: return "Graves"
-                        }
-                    }
-
-                    public var url: URL {
-                        let rootURLString = "https://github.com/rodericj/BordeauxWineRegions/raw/master/"
-                        let fileName: String
-                        switch self {
-                        case .stJulien:
-                            fileName = "St-Julien-AOP_Bordeaux_France.geojson"
-                        case .medoc:
-                            fileName = "Medoc-AOP_Bordeaux_France.geojson"
-                        case .hautMedoc:
-                            fileName = "Haut-Medoc-AOP_Bordeaux_France.geojson"
-                        case .stEsteph:
-                            fileName = "St-Estephe-AOP_Bordeaux_France.geojson"
-                        case .margaux:
-                            fileName = "Margaux-AOP_Bordeaux_France.geojson"
-                        case .moulisEnMedoc:
-                            fileName = "Moulis-en-Medoc-AOP_Bordeaux_France.geojson"
-                        case .pauillac:
-                            fileName = "Pauillac-AOP_Bordeaux_France.geojson"
-                        case .sauternes:
-                            fileName = "Sauternes-PDO_Bordeaux_France.geojson"
-                        case .graves:
-                            fileName = "Graves-AOP_Bordeaux_France.geojson"
-                        }
-                        return URL(string: "\(rootURLString)/\(fileName)")!
-                    }
-                    case stJulien = "St. Julien"
-                    case medoc
-                    case hautMedoc = "Haut-Médoc"
-                    case stEsteph = "St. Estèphe"
-                    case margaux = "Margaux"
-                    case moulisEnMedoc = "Moulis en Médoc"
-                    case pauillac = "Pauillac"
-                    case sauternes = "Sauternes"
-                    case graves = "Graves"
-                }
+                case stJulien = "St. Julien"
+                case medoc
+                case hautMedoc = "Haut-Médoc"
+                case stEsteph = "St. Estèphe"
+                case margaux = "Margaux"
+                case moulisEnMedoc = "Moulis en Médoc"
+                case pauillac = "Pauillac"
+                case sauternes = "Sauternes"
+                case graves = "Graves"
             }
         }
     }
+
 }
---
+++ Sources/WineRegionLib/Regions/USA.swift
@@ -0,0 +1,44 @@
+//
+//  File.swift
+//  
+//
+//  Created by Roderic Campbell on 12/5/20.
+//
+
+import Foundation
+
+public struct USA {
+    public static let title = "USA"
+    public struct California {
+        public static let title = "California"
+        public enum Appelation: String, AppelationDescribable {
+
+            // This maps to the geoJson field ava_id
+            public var description: String {
+                switch self {
+                case .napa:
+                    return "napa_valley"
+                //                    case .santaCruz:
+                //                        return "santa_cruz_mountains"
+                case .centralCoast:
+                    return "central_coast"
+                //                    case .saintHelena:
+                //                        return "saint__helena"
+                case .mendocinoCounty:
+                    return "mendocino"
+                case .dryCreekValley:
+                    return "dry_creek_valley"
+                }
+            }
+
+
+            public var url: URL {
+                return URL(string: "https://github.com/rodericj/ava/raw/master/avas_by_state/CA_avas.geojson")!
+            }
+            case mendocinoCounty = "Mendocino County"
+            case napa = "Napa Valley"
+            case centralCoast
+            case dryCreekValley = "Dry Creek Valley"
+        }
+    }
+}
