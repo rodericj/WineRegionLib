df6eb17e9ed01ebff2755a1cf9d4a1aa2da083ad
Roderic Campbell <roderic@gmail.com>
2020-12-10 23:38:38 -0800

    Consolidate all responses into a dispatch group and a single publisher
    MKGeoJSONFeature and MKPolygon now conform to MapKitOverlayable
    so that we can send them all through the same publisher
    
    The responses from the data task are now collected and processed
    in the consolidate func which is what is responsible for all
    of the decoding and sending to the stream.

--- Sources/WineRegionLib/WineRegionLib.swift
+++ Sources/WineRegionLib/WineRegionLib.swift
@@ -9,6 +9,12 @@ public protocol AppelationDescribable: CustomStringConvertible {
     var url: URL { get }
 }
 
+public protocol MapKitOverlayable {}
+
+@available(iOS 13.0, *)
+extension MKGeoJSONFeature: MapKitOverlayable {}
+
+extension MKPolygon: MapKitOverlayable {}
 extension Array where Element == AppelationDescribable {
     var allKeys: [String] {
         map { locale in
@@ -42,11 +48,7 @@ struct OpenStreetMapFormat: Codable {
 
 @available(iOS 13.0, *)
 public class WineRegion: ObservableObject {
-    // TODO I believe these can be concolidated into [String: MKGeoJsonObject]
-    @Published public var regionMaps: [String: MKGeoJSONFeature] = [:]
-    @Published public var regionPolygons: [String: MKPolygon] = [:]
-    @Published private(set) var networkOutputStream: (keys: [String], temporaryFileLocation: URL?, response: URLResponse?, error: Error?)  = ([], nil, nil, nil)
-
+    @Published public var regionMaps: [MapKitOverlayable] = [] // TODO change this to a result type with loading perhaps
     let session = URLSession(configuration: .default)
 
     var networkCancellable: AnyCancellable? = nil
@@ -56,23 +58,6 @@ public class WineRegion: ObservableObject {
 
     public init() {}
 
-    public func start() {
-
-        networkCancellable = $networkOutputStream.sink { _ in } receiveValue: { (keys, tempURL, response, error) in
-            if let error = error {
-                debugPrint("An error occurred while downloading a geojson file \(error)")
-            }
-            if let response = response as? HTTPURLResponse {
-                self.handleResponseObject(response: response)
-            }
-            guard let temporaryFileLocation = tempURL else {
-                return
-            }
-            print(temporaryFileLocation)
-            self.processDownloadedContent(temporaryFileLocation, keys: keys, with: self.decoder)
-        }
-    }
-
     private func handleResponseObject(response: HTTPURLResponse) {
         switch response.statusCode {
         case 200...299:
@@ -80,52 +65,85 @@ public class WineRegion: ObservableObject {
         case 300...399:
             break // Not much of a problem
         case 400...499:
-            print(response.statusCode) // My problem
+            debugPrint(response.statusCode) // My problem
         case 500...599:
-            print(response.statusCode) // Server problem
+            debugPrint(response.statusCode) // Server problem
         default:
             debugPrint("unknown status code \(response.statusCode)")
         }
     }
 
-    fileprivate func processDownloadedContent(_ temporaryFileLocation: URL, keys: [String], with decoder: MKGeoJSONDecoder) {
-        do {
-            let data = try Data(contentsOf: temporaryFileLocation)
-            let objects = try decoder.decode(data)
-
-            // split here
-            if let polygons = objects as? [MKPolygon] {
-                let newPolygons = Dictionary(uniqueKeysWithValues: zip(keys, polygons)).merging(regionPolygons) { (current, new) -> MKPolygon in
-                    new
-                }
-                self.regionPolygons = newPolygons
-
-            }
-            if let mkGeoJSONObjects = objects as? [MKGeoJSONFeature] {
-                let newRegions = Dictionary(uniqueKeysWithValues: zip(keys, mkGeoJSONObjects)).merging(regionMaps) { (current, new) -> MKGeoJSONFeature in
-                    new
-                }
-                self.regionMaps = newRegions
-            }
-        }
-        catch {
-            print("error decoding or getting file \(error)")
-        }
-    }
-
-    private func fetchFrom(urls: [URL], keys: [String]) {
+    private func fetchFrom(urls: [URL]) {
         DispatchQueue.global(qos: .background).async {
+            var datum = [Data]()
+            let dispatchGroup = DispatchGroup()
+
             urls.map { url in
                 URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad)
             }.forEach { request in
+                dispatchGroup.enter()
+                debugPrint("fetching \(request.url!)")
                 let task = self.session.downloadTask(with: request) { [weak self] (temporaryFileLocation, response, error)  in
-                    self?.networkOutputStream = (keys: keys, temporaryFileLocation: temporaryFileLocation, response: response, error: error)
+                    debugPrint("got \(request.url!)")
+
+                    if let error = error {
+                        debugPrint("An error occurred while downloading a geojson file \(error)")
+                    }
+                    if let response = response as? HTTPURLResponse {
+                        self?.handleResponseObject(response: response)
+                    }
+                    guard let temporaryFileLocation = temporaryFileLocation else {
+                        dispatchGroup.leave()
+                        return
+                    }
+                    do {
+                        debugPrint("reading temp file")
+                        let data = try Data(contentsOf: temporaryFileLocation)
+                        debugPrint("✅ successfully read temp file")
+                        guard let url = response?.url else {
+                            debugPrint("🔴 failed to read temp file")
+                            dispatchGroup.leave()
+                            return
+                        }
+                        debugPrint("✅ set it \(url.absoluteString)")
+                        datum.append(data)
+                        dispatchGroup.leave()
+                    } catch {
+                        debugPrint("🔴 to read temp file")
+                        dispatchGroup.leave()
+                    }
                 }
                 task.resume()
             }
+            dispatchGroup.notify(queue: DispatchQueue.global()) {
+                self.consolidate(datum: datum)
+            }
         }
     }
+
+    private func consolidate(datum: [Data]) {
+        debugPrint("we got \(datum.count) urls worth of data")
+        let decoder = MKGeoJSONDecoder()
+        let newRegions = datum
+            .map { theData -> [MKGeoJSONObject]? in
+                do {
+                    debugPrint("🕕 parsing")
+                    let decoded = try decoder.decode(theData)
+                    return decoded
+                } catch {
+                    debugPrint("🔴error decoding \(error)")
+                    return nil
+                }
+            }
+            .compactMap { $0 }
+            .reduce([], +)
+            .map { $0 as? MapKitOverlayable }
+            .compactMap { $0 }
+        debugPrint("new regions \(newRegions.count)")
+        self.regionMaps = newRegions
+
+    }
     public func getRegions(regions: [AppelationDescribable]) {
-        fetchFrom(urls: regions.map { $0.url }, keys: regions.allKeys)
+        fetchFrom(urls: regions.map { $0.url })
     }
 }
