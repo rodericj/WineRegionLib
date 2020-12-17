647ee26e1d717a31d56733aa155f26d0b91e4baf
Roderic Campbell <roderic@gmail.com>
2020-12-16 23:39:47 -0800

    Set up the USA data to be more hierarchical

--- Sources/WineRegionLib/Regions/USA.swift
+++ Sources/WineRegionLib/Regions/USA.swift
@@ -7,37 +7,59 @@
 
 import Foundation
 
+fileprivate extension String {
+    var appelationDescription: String {
+        // Ultimately this capitalizes the string and splits on the capitals so "abcDeFG" becomes "Abc De Fg"
+        capitalizeAndSplit()
+            .capitalized
+            .replacingOccurrences(of: "P D O", with: "PDO")
+            .replacingOccurrences(of: "_", with: "")
+            .replacingOccurrences(of: "  ", with: " ")
+    }
+
+    var appelationURL: URL {
+        let root = "https://raw.githubusercontent.com/rodericj/ava/master/avas/"
+        return URL(string: "\(root)\(self).geojson")!
+    }
+}
+
 public struct USA {
     public static let title = "USA"
     public struct California {
-        public static let title = "California"
-        public enum Appelation: String, AppelationDescribable, CaseIterable {
-            public var description: String {
-                // Ultimately this capitalizes the string and splits on the capitals so "abcDeFG" becomes "Abc De Fg"
-                let doctoredString = rawValue.capitalizeAndSplit()
-                return doctoredString
-                    .capitalized
-                    .replacingOccurrences(of: "P D O", with: "PDO")
-                    .replacingOccurrences(of: "_", with: "")
-                    .replacingOccurrences(of: "  ", with: " ")
-            }
+        public struct Napa {
+            public enum Appelation: String, AppelationDescribable, CaseIterable {
 
-            public var url: URL {
-                let root = "https://raw.githubusercontent.com/rodericj/ava/master/avas/"
-                return URL(string: "\(root)\(rawValue).geojson")!
+                public var description: String { rawValue.appelationDescription }
+                public var url: URL { rawValue.appelationURL }
+                
+                case napaValley = "napa_valley"
+                case stHelena = "st__helena"
+                case losCarneros = "los_carneros"
             }
-            case mendocino
-            case napaValley = "napa_valley"
-            case dryCreekValley = "dry_creek_valley"
-            case stHelena = "st__helena"
-            case northernSonoma = "northern_sonoma"
-            case oakville
-            case pasoRobles = "paso_robles"
-            case petalumaGap = "petaluma_gap"
-            case losCarneros = "los_carneros"
-            case santaYnezValley = "santa_ynez_valley"
-            case sonomaValley = "sonoma_valley"
-            case alexanderValley = "alexander_valley"
+        }
+        public struct Sonoma {
+            public enum Appelation: String, AppelationDescribable, CaseIterable {
+                public var description: String { rawValue.appelationDescription }
+                public var url: URL { rawValue.appelationURL }
+
+                case sonomaValley = "sonoma_valley"
+                case northernSonoma = "northern_sonoma"
+                case oakville
+                case dryCreekValley = "dry_creek_valley"
+                case petalumaGap = "petaluma_gap"
+                case alexanderValley = "alexander_valley"
+                case mendocino
+            }
+        }
+
+        public struct CentralCoast {
+            public enum Appelation: String, AppelationDescribable, CaseIterable {
+                public var description: String { rawValue.appelationDescription }
+                public var url: URL { rawValue.appelationURL }
+
+                case pasoRobles = "paso_robles"
+                case santaYnezValley = "santa_ynez_valley"
+            }
         }
     }
 }
