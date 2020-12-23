3b49a1e4f4280de50bef584e305c359b7052c08a
Roderic Campbell <roderic@gmail.com>
2020-12-22 21:31:03 -0800

    Enable sending up the region tree

--- Sources/WineRegionLib/WineRegionLib.swift
+++ Sources/WineRegionLib/WineRegionLib.swift
@@ -20,15 +20,16 @@ extension MKPolygon: MapKitOverlayable {}
 @available(iOS 13.0, *)
 extension MKMultiPolygon: MapKitOverlayable {}
 
-public enum RegionResult {
-    case regions([MapKitOverlayable])
+public enum RegionResult<T> {
+    case regions([T])
     case loading(Float)
     case none
 }
 
 @available(iOS 13.0, *)
 public class WineRegion: ObservableObject {
-    @Published public var regionMaps: RegionResult = .none
+    @Published public var regionMaps: RegionResult<MapKitOverlayable> = .none
+    @Published public var regionsTree: RegionResult<RegionJson> = .none
 
     let session = URLSession(configuration: .default)
 
@@ -78,7 +79,7 @@ public class WineRegion: ObservableObject {
                     }
                     do {
                         currentProgress += progressIncrement
-                        self?.update(result: .loading(currentProgress))
+                        self?.update(result: RegionResult<MapKitOverlayable>.loading(currentProgress))
                         let data = try Data(contentsOf: temporaryFileLocation)
                         guard let _ = response?.url else {
                             debugPrint("🔴 failed to read temp file")
@@ -114,7 +115,7 @@ public class WineRegion: ObservableObject {
                 do {
                     let decoded = try decoder.decode(theData.value)
                     currentLoadAmount += increment
-                    self.update(result: .loading(currentLoadAmount))
+                    self.update(result: RegionResult<MapKitOverlayable>.loading(currentLoadAmount))
                     return decoded
                 } catch {
                     debugPrint("🔴error decoding \(error) on \(theData.key)")
@@ -126,15 +127,75 @@ public class WineRegion: ObservableObject {
             .map { $0 as? MapKitOverlayable }
             .compactMap { $0 }
         debugPrint("new regions \(newRegions.count)")
-        update(result: .regions(newRegions))
+        update(result: RegionResult<MapKitOverlayable>.regions(newRegions))
     }
 
-    private func update(result: RegionResult) {
+    private func update(result: RegionResult<MapKitOverlayable>) {
         DispatchQueue.main.async {
             self.regionMaps = result
         }
     }
+
+    public func loadMap(for region: RegionJson) {
+        guard let url = URL(string: region.url) else {
+            print("not a valid url \(region.url)")
+            return
+        }
+        fetchFrom(urls: [url])
+    }
+
     public func getRegions(regions: [AppelationDescribable]) {
         fetchFrom(urls: regions.map { $0.url })
     }
+
+    public func getRegionTree() throws {
+        let decoder = JSONDecoder()
+
+        regionsTree = .loading(0)
+        // find file named "newCalifornia"
+        let californiaURL = URL(fileURLWithPath: "/Users/roderic/dev/rodericj/WineRegionLib/Scripts/newCalifornia.json")
+        let californiaData = try Data(contentsOf: californiaURL)
+        let californiaRegion = try decoder.decode(RegionJson.self, from: californiaData)
+        regionsTree = .loading(0.5)
+
+        // find file named "newItaly"
+        let italyURL = URL(fileURLWithPath: "/Users/roderic/dev/rodericj/WineRegionLib/Scripts/newItaly.json")
+        let italyData = try Data(contentsOf: italyURL)
+        let italyRegion = try decoder.decode(RegionJson.self, from: italyData)
+        regionsTree = .regions([italyRegion, californiaRegion])
+    }
+}
+
+@propertyWrapper
+public struct DecodableUUID {
+    public var wrappedValue = UUID()
+    public init() {
+        wrappedValue = UUID()
+    }
+}
+
+extension DecodableUUID: Decodable {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        wrappedValue = try container.decode(UUID.self)
+    }
+}
+
+extension KeyedDecodingContainer {
+    func decode(_ type: DecodableUUID.Type,
+                forKey key: Key) throws -> DecodableUUID {
+        try decodeIfPresent(type, forKey: key) ?? .init()
+    }
+}
+public struct RegionJson: Decodable, Identifiable {
+    @DecodableUUID public var id: UUID
+    public let title: String
+    public let url: String
+    public let children: [RegionJson]?
+    public init(title: String, url: String, children: [RegionJson]?) {
+        self.title = title
+        self.url = url
+        self.children = children
+    }
+
 }
