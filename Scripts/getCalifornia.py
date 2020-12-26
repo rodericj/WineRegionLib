236f20e274f07af8ddf73af03e5a6e05093b67e8
Roderic Campbell <roderic@gmail.com>
2020-12-26 12:30:43 -0800

    Update getCalifornia to also get Oregon, create USA

--- Scripts/getCalifornia.py
+++ Scripts/getCalifornia.py
@@ -4,13 +4,6 @@ from anytree import NodeMixin, RenderTree, Node
 from anytree import Node, RenderTree, AsciiStyle
 from anytree.exporter import JsonExporter
 
-fileName = '/Users/roderic/dev/rodericj/WineRegionLib/Scripts/CA_avas.geojson'
-file = open(fileName, 'r')
-d = json.load(file)
-
-allFeatures = d["features"]
-
-
 ## This is the anytree implementation
 
 class MyBaseClass(object):
@@ -26,36 +19,88 @@ class AVAFeatureNode(MyBaseClass, NodeMixin):  # Add Node feature
         if children:
             self.children = children
 
-californiaNode = AVAFeatureNode("California", "none",
-                                "https://raw.githubusercontent.com/rodericj/ava/master/avas/" + "californiaPlaceholderTODO" + ".geojson")
-for feature in allFeatures:
-    name = feature["properties"]["name"]
-    avaID = feature["properties"]["ava_id"]
-    url = "https://raw.githubusercontent.com/rodericj/ava/master/avas/" + avaID.replace(" ", "") + ".geojson"
+# Straight up copy paste of California
+def getOregon():
+    oregonFileName = '/Users/roderic/dev/rodericj/WineRegionLib/Scripts/OR_avas.geojson'
+    oregonFile = open(oregonFileName, 'r')
+    oregonData = json.load(oregonFile)
 
-    thisfeatureAsNode = AVAFeatureNode(name, avaID, url)
-    if feature["properties"]["within"] is None:
-        thisfeatureAsNode.parent = californiaNode
-        continue
+    allFeatures = oregonData["features"]
 
-for feature in allFeatures:
-    if feature["properties"]["within"] is None:
-        continue  # because we've already added it
-    name = feature["properties"]["name"]
-    avaID = feature["properties"]["ava_id"]
-    url = "https://raw.githubusercontent.com/rodericj/ava/master/avas/" + avaID.replace(" ", "") + ".geojson"
+    oregonNode = AVAFeatureNode("Oregon", "none",
+                                    "https://raw.githubusercontent.com/rodericj/WineRegionMaps/main/USA/Oregon.geojson")
+    for feature in allFeatures:
+        name = feature["properties"]["name"]
+        avaID = feature["properties"]["ava_id"]
+        url = "https://raw.githubusercontent.com/rodericj/ava/master/avas/" + avaID.replace(" ", "") + ".geojson"
 
-    withinArray = feature["properties"]["within"].split("|")
-    allWithin = anytree.findall(californiaNode, filter_=lambda node: node.name in withinArray)
-    filtered_lst = [(x, y) for x, y in enumerate(allWithin)]
+        thisfeatureAsNode = AVAFeatureNode(name, avaID, url)
+        if feature["properties"]["within"] is None:
+            thisfeatureAsNode.parent = oregonNode
+            continue
 
-    if len(filtered_lst) == 0:
-        continue
-    thisfeatureAsNode = AVAFeatureNode(name, avaID, url, parent=max(filtered_lst)[1])
+    for feature in allFeatures:
+        if feature["properties"]["within"] is None:
+            continue  # because we've already added it
+        name = feature["properties"]["name"]
+        avaID = feature["properties"]["ava_id"]
+        url = "https://raw.githubusercontent.com/rodericj/ava/master/avas/" + avaID.replace(" ", "") + ".geojson"
+
+        withinArray = feature["properties"]["within"].split("|")
+        allWithin = anytree.findall(californiaNode, filter_=lambda node: node.name in withinArray)
+        filtered_lst = [(x, y) for x, y in enumerate(allWithin)]
+
+        if len(filtered_lst) == 0:
+            continue
+        thisfeatureAsNode = AVAFeatureNode(name, avaID, url, parent=max(filtered_lst)[1])
+    return oregonNode
+
+def getCalifornia():
+    californiaFileName = '/Users/roderic/dev/rodericj/WineRegionLib/Scripts/CA_avas.geojson'
+    californiaFile = open(californiaFileName, 'r')
+    californiaData = json.load(californiaFile)
+
+    allFeatures = californiaData["features"]
+    californiaNode = AVAFeatureNode("California", "none",
+                                    "https://raw.githubusercontent.com/rodericj/WineRegionMaps/main/USA/California.geojson")
+    for feature in allFeatures:
+        name = feature["properties"]["name"]
+        avaID = feature["properties"]["ava_id"]
+        url = "https://raw.githubusercontent.com/rodericj/ava/master/avas/" + avaID.replace(" ", "") + ".geojson"
+
+        thisfeatureAsNode = AVAFeatureNode(name, avaID, url)
+        if feature["properties"]["within"] is None:
+            thisfeatureAsNode.parent = californiaNode
+            continue
+
+    for feature in allFeatures:
+        if feature["properties"]["within"] is None:
+            continue  # because we've already added it
+        name = feature["properties"]["name"]
+        avaID = feature["properties"]["ava_id"]
+        url = "https://raw.githubusercontent.com/rodericj/ava/master/avas/" + avaID.replace(" ", "") + ".geojson"
+
+        withinArray = feature["properties"]["within"].split("|")
+        allWithin = anytree.findall(californiaNode, filter_=lambda node: node.name in withinArray)
+        filtered_lst = [(x, y) for x, y in enumerate(allWithin)]
+
+        if len(filtered_lst) == 0:
+            continue
+        thisfeatureAsNode = AVAFeatureNode(name, avaID, url, parent=max(filtered_lst)[1])
+    return californiaNode
+
+
+usa = AVAFeatureNode("USA", "none", "")
+
+californiaNode = getCalifornia()
+oregonNode = getOregon()
+
+californiaNode.parent = usa
+oregonNode.parent = usa
 
 # for pre, fill, node in RenderTree(californiaNode):
 #     treestr = u"%s%s" % (pre, node.name)
 #     print(treestr.ljust(8), node.url)
 exporter = JsonExporter(indent=2, sort_keys=True)
 
-print(exporter.export(californiaNode))
+print(exporter.export(usa))
